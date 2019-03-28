3eae6f1055c825311264a021b0591265c943384d
Sam Goldstein <sam@stackery.io>
2019-03-28 10:56:08 -0700

    Add api response

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,5 +1,12 @@
 exports.handler = async message => {
   console.log(message);
 
-  return {};
-}
+  const response = {
+    statusCode: '204',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: `Success\n${JSON.stringify(message, undefined, 2)}`
+  };
+  return response;
+};
